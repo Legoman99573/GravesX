2fc86526039396e1cb1ac550e60220ad0c2df556
Legoman99573 <legoman@ugandavrc.com>
2024-08-01 00:55:30 -0500

    Bump Spigot and Paper to 1.21-R0.1-SNAPSHOT
    Time to hunt down this shit >:(

--- src/main/java/com/ranull/graves/manager/RecipeManager.java
+++ src/main/java/com/ranull/graves/manager/RecipeManager.java
@@ -7,11 +7,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.Recipe;
-import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.*;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
