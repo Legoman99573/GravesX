62f707dd88caa0a39baf263101d2f2a8039edd9d
Legoman99573 <legoman@ugandavrc.com>
2024-08-16 22:56:11 -0500

    Fix piston logic to not affect redstone contraptions
    Closes #9

--- src/main/java/com/ranull/graves/listener/BlockPistonExtendListener.java
+++ src/main/java/com/ranull/graves/listener/BlockPistonExtendListener.java
@@ -2,11 +2,13 @@ package com.ranull.graves.listener;
 
 import com.ranull.graves.Graves;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPistonExtendEvent;
+
+import java.util.List;
 
 /**
  * Listens for BlockPistonExtendEvent to prevent pistons from moving blocks that are graves or are near holograms of graves.
@@ -30,20 +32,43 @@ public class BlockPistonExtendListener implements Listener {
      */
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockPistonExtend(BlockPistonExtendEvent event) {
-        Block block = event.getBlock().getRelative(event.getDirection());
+        BlockFace direction = event.getDirection();
 
-        // Check if the block being moved is part of a grave
-        if (plugin.getBlockManager().getGraveFromBlock(block) != null) {
-            event.setCancelled(true);
-        } else {
-            // Check for entities within a 12-block radius around the block
-            double radius = 12.0;
-            for (Entity entity : block.getWorld().getNearbyEntities(block.getLocation(), radius, radius, radius)) {
-                if (plugin.getHologramManager().getGrave(entity) != null) {
-                    event.setCancelled(true);
-                    break; // No need to continue checking if we've found a grave hologram
-                }
+        Block piston = event.getBlock();
+        // Get all blocks being moved by the piston
+        List<Block> blocks = event.getBlocks();
+
+        BlockFace[] facesToCheck = {BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST, BlockFace.UP, BlockFace.DOWN};
+
+        // Check all faces around the piston
+        for (BlockFace face : facesToCheck) {
+            Block adjacentBlock = piston.getRelative(face);
+
+            // Check if the adjacent block is part of a grave
+            if (plugin.getBlockManager().getGraveFromBlock(adjacentBlock) != null) {
+                event.setCancelled(true);
+                return;
             }
+        }
+
+        // Check each block to see if it's within 12 blocks of a grave or grave hologram
+        for (Block block : blocks) {
+            Block relativeBlock = block.getRelative(direction);
+
+            // Check if the block being moved is part of a grave
+            if (plugin.getBlockManager().getGraveFromBlock(relativeBlock) != null) {
+                event.setCancelled(true);
+                return;
+            }
+
+            // Check for entities (such as holograms) within a 12-block radius around the block
+            // double radius = 12.0;
+            // for (Entity entity : relativeBlock.getWorld().getNearbyEntities(relativeBlock.getLocation(), radius, radius, radius)) {
+            //     if (plugin.getHologramManager().getGrave(entity) != null) {
+            //         event.setCancelled(true);
+            //         return;
+            //     }
+            // }
         }
     }
 }
