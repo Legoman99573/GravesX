2cf782d90f42bb43188774b56ad08dc8ff9d3f37
Christian W. <7460704+Legoman99573@users.noreply.github.com>
2024-10-23 00:43:57 -0500

    Update MySQL Connector dependency to 9.1.0

--- src/main/java/com/ranull/graves/Graves.java
+++ src/main/java/com/ranull/graves/Graves.java
@@ -230,7 +230,7 @@ public class Graves extends JavaPlugin {
 
         libraryLoaderUtil.loadLibrary("org{}postgresql", "postgresql", "42.7.4", "org{}postgresql", "com{}ranull{}graves{}libraries{}postgresql", false);
         libraryLoaderUtil.loadLibrary("org{}mariadb{}jdbc", "mariadb-java-client", "3.4.1", "org{}mariadb", "com{}ranull{}graves{}libraries{}mariadb", false);
-        libraryLoaderUtil.loadLibrary("com{}mysql", "mysql-connector-j", "9.0.0", false);
+        libraryLoaderUtil.loadLibrary("com{}mysql", "mysql-connector-j", "9.1.0", false);
         libraryLoaderUtil.loadLibrary("com{}h2database", "h2", "2.3.232", "org{}h2", "com{}ranull{}graves{}libraries{}h2", false);
         //libraryLoaderUtil.loadLibrary("org{}slf4j", "slf4j-simple", "2.0.16", false);
         libraryLoaderUtil.loadLibrary("net{}kyori", "adventure-api", "4.17.0", "net{}kyori", "com{}ranull{}graves{}libraries{}kyori", false);
