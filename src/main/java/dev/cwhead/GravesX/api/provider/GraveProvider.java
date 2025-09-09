c7c7463f844278b4b8612aa65765b02e75b4d7e0
Christian W. <7460704+Legoman99573@users.noreply.github.com>
2025-09-09 03:53:36 -0500

    Javadoc GraveProvider's parameters

--- src/main/java/dev/cwhead/GravesX/api/provider/GraveProvider.java
+++ src/main/java/dev/cwhead/GravesX/api/provider/GraveProvider.java
@@ -21,27 +21,40 @@ public interface GraveProvider {
 
     /**
      * Create/place any custom objects for this grave at the given location.
+     *
+     * @param location the location of the grave
+     * @param grave the grave to place over the default grave
      */
     void place(Location location, Grave grave) throws Exception;
 
     /**
      * Remove any custom objects that belong to this grave.
+     *
+     * @param grave the grave to remove custom objects
      */
     void remove(Grave grave) throws Exception;
 
     /**
      * Return true if this provider detects something placed for the grave.
+     *
+     * @param grave the grave to check
+     *
+     * @return true if the grave is placed. False otherwise.
      */
     boolean isPlaced(Grave grave);
 
     /**
      * Return true if this provider recognizes the given CUSTOM entity data.
      * (E.g. check a metadata field or namespaced tag your module wrote.)
+     *
+     * @param data the Custom Entity Data
      */
     boolean supports(EntityData data);
 
     /**
      * Remove a specific CUSTOM entity data record, if supported.
+     *
+     * @param data the entity data record
      *
      * @return true if handled/removed, false to let others try.
      */
