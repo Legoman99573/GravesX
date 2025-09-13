80eaefe99a959226d0c094246fdbb66865247b78
Christian W. <7460704+Legoman99573@users.noreply.github.com>
2025-09-13 02:57:33 -0500

    Bump config version

--- src/main/java/com/ranull/graves/Graves.java
+++ src/main/java/com/ranull/graves/Graves.java
@@ -659,7 +659,7 @@ public class Graves extends JavaPlugin {
      * and replaces them with updated ones from the plugin's resources.
      */
     private void updateConfig() {
-        int currentConfigVersion = 21;
+        int currentConfigVersion = 22;
         File configFolder = new File(getDataFolder(), "config");
 
         // Load the main config file to check the version
