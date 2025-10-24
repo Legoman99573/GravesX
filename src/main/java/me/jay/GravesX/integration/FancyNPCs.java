52278632b85f1a7bb9e83f8de808a5ff0090daa7
Christian W. <7460704+Legoman99573@users.noreply.github.com>
2025-10-24 13:34:42 -0500

    Fix changes with FancyNPCs in 2.8.0+

--- src/main/java/me/jay/GravesX/integration/FancyNPCs.java
+++ src/main/java/me/jay/GravesX/integration/FancyNPCs.java
@@ -16,11 +16,13 @@ import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.EquipmentSlot;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.UUID;
 
 public final class FancyNPCs extends EntityDataManager {
-    private static final String POSE_ATTRIBUTE_NAME = "pose";
 
+    private static final String POSE_ATTRIBUTE_NAME = "pose";
     private final Graves plugin;
 
     public FancyNPCs(Graves plugin) {
@@ -30,154 +32,131 @@ public final class FancyNPCs extends EntityDataManager {
 
     public void createCorpse(UUID uuid, Location location, Grave grave) {
         plugin.getGravesXScheduler().runTask(() -> {
-            if (plugin.getConfig("fancynpcs.corpse.enabled", grave).getBoolean("fancynpcs.corpse.enabled")
-                    && grave.getOwnerType() == EntityType.PLAYER) {
+            if (!plugin.getConfig("fancynpcs.corpse.enabled", grave).getBoolean("fancynpcs.corpse.enabled")
+                    || grave.getOwnerType() != EntityType.PLAYER) return;
 
-                location.getBlock().setType(Material.AIR);
-                Location npcLocation = location.clone();
+            location.getBlock().setType(Material.AIR);
+            Location npcLocation = location.clone();
 
-                String ownerIdStr = grave.getOwnerUUID().toString().replace("-", "");
-                String npcIdStr = uuid.toString().replace("-", "");
-
-                NpcManager npcManager = FancyNpcsPlugin.get().getNpcManager();
-                try {
-                    double x = plugin.getConfig("fancynpcs.corpse.offset.x", grave).getDouble("fancynpcs.corpse.offset.x");
-                    double y = plugin.getConfig("fancynpcs.corpse.offset.y", grave).getDouble("fancynpcs.corpse.offset.y");
-                    double z = plugin.getConfig("fancynpcs.corpse.offset.z", grave).getDouble("fancynpcs.corpse.offset.z");
-                    npcLocation.add(x, y, z);
-                } catch (IllegalArgumentException handled) {
-                    npcLocation.add(-0.5, 0, -0.5);
-                }
-
-                NpcData newNpcData = new NpcData(npcIdStr, grave.getOwnerUUID(), npcLocation);
-                SkinData skin = new SkinData(ownerIdStr, SkinData.SkinVariant.AUTO, grave.getOwnerTexture(), grave.getOwnerTextureSignature());
-
-                Npc newNpc = FancyNpcsPlugin.get().getNpcAdapter().apply(newNpcData);
-                newNpc.getData().setSkin(String.valueOf(skin));
-                newNpc.create();
-                npcManager.registerNpc(newNpc);
-                newNpc.spawnForAll();
-                newNpc.update(Bukkit.getPlayer(grave.getOwnerUUID()));
-                newNpc.updateForAll();
-
-                Npc npc = npcManager.getNpc(npcIdStr);
-                NpcAttribute attribute = FancyNpcsPlugin.get().getAttributeManager().getAttributeByName(EntityType.PLAYER, POSE_ATTRIBUTE_NAME);
-                npc.getData().getAttributes().put(attribute, "sleeping");
-                npc.getData().setCollidable(false);
-                npc.getData().setShowInTab(false);
-                npc.getData().setDisplayName("<empty>");
-                npc.updateForAll();
-                npc.getData().setSkin(String.valueOf(skin));
-
-                if (plugin.getConfig("fancynpcs.corpse.armor", grave).getBoolean("fancynpcs.corpse.armor")) {
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.HEAD)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.HEAD, grave.getEquipmentMap().get(EquipmentSlot.HEAD));
-                    }
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.CHEST)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.CHEST, grave.getEquipmentMap().get(EquipmentSlot.CHEST));
-                    }
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.LEGS)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.LEGS, grave.getEquipmentMap().get(EquipmentSlot.LEGS));
-                    }
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.FEET)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.FEET, grave.getEquipmentMap().get(EquipmentSlot.FEET));
-                    }
-                }
-
-                if (plugin.getConfig("fancynpcs.corpse.hand", grave).getBoolean("fancynpcs.corpse.hand")) {
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.HAND)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.MAINHAND, grave.getEquipmentMap().get(EquipmentSlot.HAND));
-                    }
-                    if (plugin.getVersionManager().hasSecondHand() && grave.getEquipmentMap().containsKey(EquipmentSlot.OFF_HAND)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.OFFHAND, grave.getEquipmentMap().get(EquipmentSlot.OFF_HAND));
-                    }
-                }
-
-                npc.update(Bukkit.getPlayer(grave.getOwnerUUID()));
-                npc.updateForAll();
-                npc.removeForAll();
-                npc.create();
-                npc.spawnForAll();
+            try {
+                double x = plugin.getConfig("fancynpcs.corpse.offset.x", grave).getDouble("fancynpcs.corpse.offset.x");
+                double y = plugin.getConfig("fancynpcs.corpse.offset.y", grave).getDouble("fancynpcs.corpse.offset.y");
+                double z = plugin.getConfig("fancynpcs.corpse.offset.z", grave).getDouble("fancynpcs.corpse.offset.z");
+                npcLocation.add(x, y, z);
+            } catch (IllegalArgumentException ignored) {
+                npcLocation.add(-0.5, 0, -0.5);
             }
+
+            String ownerIdStr = grave.getOwnerUUID().toString();
+            String npcIdStr = uuid.toString().replace("-", "");
+
+            NpcManager npcManager = FancyNpcsPlugin.get().getNpcManager();
+            NpcData npcData = new NpcData(npcIdStr, grave.getOwnerUUID(), npcLocation);
+            Npc npc = FancyNpcsPlugin.get().getNpcAdapter().apply(npcData);
+
+            try {
+                SkinData skin = new SkinData(ownerIdStr, SkinData.SkinVariant.AUTO, toValidBase64Texture(grave.getOwnerTexture()), grave.getOwnerTextureSignature());
+                npc.getData().setSkin(skin.getIdentifier());
+            } catch (Exception ex) {
+                plugin.getLogger().warning("Failed to load NPC skin for grave " + grave.getUUID() + ": " + ex.getMessage());
+                plugin.logStackTrace(ex);
+            }
+
+            npc.create();
+            npcManager.registerNpc(npc);
+            npc.spawnForAll();
+
+            NpcAttribute poseAttribute = FancyNpcsPlugin.get().getAttributeManager().getAttributeByName(EntityType.PLAYER, POSE_ATTRIBUTE_NAME);
+            npc.getData().getAttributes().put(poseAttribute, "sleeping");
+            npc.getData().setCollidable(false);
+            npc.getData().setShowInTab(false);
+            npc.getData().setDisplayName("<empty>");
+
+            // Equip armor
+            if (plugin.getConfig("fancynpcs.corpse.armor", grave).getBoolean("fancynpcs.corpse.armor")) {
+                equipArmor(npc, grave);
+            }
+
+            // Equip hands
+            if (plugin.getConfig("fancynpcs.corpse.hand", grave).getBoolean("fancynpcs.corpse.hand")) {
+                equipHands(npc, grave);
+            }
+
+            npc.updateForAll();
         });
     }
 
     public void createBedrockcompatCorpse(UUID uuid, Location location, Grave grave) {
         plugin.getGravesXScheduler().runTask(() -> {
-            if (plugin.getConfig("fancynpcs.corpse.enabled", grave).getBoolean("fancynpcs.corpse.enabled")
-                    && grave.getOwnerType() == EntityType.PLAYER) {
+            if (!plugin.getConfig("fancynpcs.corpse.enabled", grave).getBoolean("fancynpcs.corpse.enabled")
+                    || grave.getOwnerType() != EntityType.PLAYER) return;
 
-                location.getBlock().setType(Material.AIR);
-                Location npcLocation = location.clone();
+            location.getBlock().setType(Material.AIR);
+            Location npcLocation = location.clone();
 
-                String npcIdStr = uuid.toString().replace("-", "");
-
-                NpcManager npcManager = FancyNpcsPlugin.get().getNpcManager();
-                try {
-                    double x = plugin.getConfig("fancynpcs.corpse.offset.x", grave).getDouble("fancynpcs.corpse.offset.x");
-                    double y = plugin.getConfig("fancynpcs.corpse.offset.y", grave).getDouble("fancynpcs.corpse.offset.y");
-                    double z = plugin.getConfig("fancynpcs.corpse.offset.z", grave).getDouble("fancynpcs.corpse.offset.z");
-                    npcLocation.add(x, y, z);
-                } catch (IllegalArgumentException handled) {
-                    npcLocation.add(-0.5, 0, -0.5);
-                }
-
-                NpcData newNpcData = new NpcData(grave.getUUID().toString().replace("-", ""), grave.getOwnerUUID(), npcLocation);
-                SkinData skin = new SkinData(
-                        "194ffca812294de7ab5386bb5c2686d3",
-                        SkinData.SkinVariant.AUTO,
-                        "ewogICJ0aW1lc3RhbXAiIDogMTcwMDA3NTcyMjAzOSwKICAicHJvZmlsZUlkIiA6ICIxOTRmZmNhODEyMjk0ZGU3YWI1Mzg2YmI1YzI2ODZkMyIsCiAgInByb2ZpbGVOYW1lIiA6ICJDb3Jwc2UiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNWJmZjJlYzQ0ZWM1MWIzMmVmMzc4YTY1NzIwYzA2MGYzYWRmNzQ5NDVkNDgwNmQ1YjQyZTI1Y2UxNzM2NGViZiIKICAgIH0KICB9Cn0=",
-                        "v2WrGsMU53dyK1xvx6xS5r41XM4mvR6tB/86Tf5CjtQtv5ozjhEaHARHqFChnTl4/oG238alBMoFw6punEdLLJ8vVYSAa0K8CSpm8RT/gGvxpd6JHGsvcOEWEOV2wv0cntBs9BgrvoKvdFz7WyzT7w1PyP/74waU/Z83lBMU9he71DOFgAVnWXIp2PIWttK89hpbSmkrrdMLQ18/bUURQnp082ZinlDa7G2OjRbdpxGluOCKU725rufdnMhMBj5FCuuW8FaApa+6vuDDg6puIJgOXwtRX5/ZTp22UwEaMSegM+aP7oENx3wmm6XHHs3fgsulquRmxDuhAZ+sMi8wnW6lZU+2FWpsIOh4Xehn426iDu5wl4/kFe4RzTXr7G6N4uncgDRVaQQwsM3L/A7TmRbs8rQVrphqhOMvZ5R9fVu668EbMtAJbobofNxsVTRsRA9o7jnusIhmrWwroqVVxpq4k517ZEzDbPHkH/2X/amc7IGoeSLLfngIRYD+n7EUzO5ErQWFS778DiCxtQHKNOrBc/D+Fg9HsoH/Z2rD5dUBcxQ5DhprgMGGbaLDoQXjFul0mkE4Rg5yubonK+Ccvwmtv2s37sj1FwEJwllSFxvhmjxifTjSCaVoXJnsGJEZf3Zok9g2qk9gBzbgM1V2Ub8iOMupRs4JET9WR8+XIEk="
-                );
-
-                Npc newNpc = FancyNpcsPlugin.get().getNpcAdapter().apply(newNpcData);
-                newNpc.getData().setSkin(String.valueOf(skin));
-                newNpc.create();
-                npcManager.registerNpc(newNpc);
-                newNpc.spawnForAll();
-                newNpc.update(Bukkit.getPlayer(grave.getOwnerUUID()));
-                newNpc.updateForAll();
-
-                Npc npc = npcManager.getNpc(npcIdStr);
-                NpcAttribute attribute = FancyNpcsPlugin.get().getAttributeManager().getAttributeByName(EntityType.PLAYER, POSE_ATTRIBUTE_NAME);
-                npc.getData().getAttributes().put(attribute, "sleeping");
-                npc.getData().setCollidable(false);
-                npc.getData().setShowInTab(false);
-                npc.getData().setDisplayName("<empty>");
-                npc.updateForAll();
-                npc.getData().setSkin(String.valueOf(skin));
-
-                if (plugin.getConfig("fancynpcs.corpse.armor", grave).getBoolean("fancynpcs.corpse.armor")) {
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.HEAD)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.HEAD, grave.getEquipmentMap().get(EquipmentSlot.HEAD));
-                    }
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.CHEST)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.CHEST, grave.getEquipmentMap().get(EquipmentSlot.CHEST));
-                    }
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.LEGS)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.LEGS, grave.getEquipmentMap().get(EquipmentSlot.LEGS));
-                    }
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.FEET)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.FEET, grave.getEquipmentMap().get(EquipmentSlot.FEET));
-                    }
-                }
-
-                if (plugin.getConfig("fancynpcs.corpse.hand", grave).getBoolean("fancynpcs.corpse.hand")) {
-                    if (grave.getEquipmentMap().containsKey(EquipmentSlot.HAND)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.MAINHAND, grave.getEquipmentMap().get(EquipmentSlot.HAND));
-                    }
-                    if (plugin.getVersionManager().hasSecondHand() && grave.getEquipmentMap().containsKey(EquipmentSlot.OFF_HAND)) {
-                        npc.getData().addEquipment(NpcEquipmentSlot.OFFHAND, grave.getEquipmentMap().get(EquipmentSlot.OFF_HAND));
-                    }
-                }
-
-                npc.update(Bukkit.getPlayer(grave.getOwnerUUID()));
-                npc.updateForAll();
-                npc.removeForAll();
-                npc.create();
-                npc.spawnForAll();
+            try {
+                double x = plugin.getConfig("fancynpcs.corpse.offset.x", grave).getDouble("fancynpcs.corpse.offset.x");
+                double y = plugin.getConfig("fancynpcs.corpse.offset.y", grave).getDouble("fancynpcs.corpse.offset.y");
+                double z = plugin.getConfig("fancynpcs.corpse.offset.z", grave).getDouble("fancynpcs.corpse.offset.z");
+                npcLocation.add(x, y, z);
+            } catch (IllegalArgumentException ignored) {
+                npcLocation.add(-0.5, 0, -0.5);
             }
+
+            String npcIdStr = uuid.toString().replace("-", "");
+            String ownerIdStr = grave.getOwnerUUID().toString();
+
+            NpcManager npcManager = FancyNpcsPlugin.get().getNpcManager();
+            NpcData npcData = new NpcData(npcIdStr, grave.getOwnerUUID(), npcLocation);
+            Npc npc = FancyNpcsPlugin.get().getNpcAdapter().apply(npcData);
+
+            try {
+                npc.getData().setSkin(ownerIdStr);
+            } catch (Exception ex) {
+                plugin.getLogger().warning("Failed to apply Bedrock skin for grave " + grave.getUUID() + ": " + ex.getMessage());
+                plugin.logStackTrace(ex);
+            }
+
+            npc.create();
+            npcManager.registerNpc(npc);
+            npc.spawnForAll();
+
+            NpcAttribute poseAttribute = FancyNpcsPlugin.get().getAttributeManager()
+                    .getAttributeByName(EntityType.PLAYER, POSE_ATTRIBUTE_NAME);
+            npc.getData().getAttributes().put(poseAttribute, "sleeping");
+            npc.getData().setCollidable(false);
+            npc.getData().setShowInTab(false);
+            npc.getData().setDisplayName("<empty>");
+
+            if (plugin.getConfig("fancynpcs.corpse.armor", grave).getBoolean("fancynpcs.corpse.armor")) {
+                equipArmor(npc, grave);
+            }
+
+            if (plugin.getConfig("fancynpcs.corpse.hand", grave).getBoolean("fancynpcs.corpse.hand")) {
+                equipHands(npc, grave);
+            }
+
+            npc.updateForAll();
         });
+    }
+
+
+    private void equipArmor(Npc npc, Grave grave) {
+        if (grave.getEquipmentMap().containsKey(EquipmentSlot.HEAD))
+            npc.getData().addEquipment(NpcEquipmentSlot.HEAD, grave.getEquipmentMap().get(EquipmentSlot.HEAD));
+        if (grave.getEquipmentMap().containsKey(EquipmentSlot.CHEST))
+            npc.getData().addEquipment(NpcEquipmentSlot.CHEST, grave.getEquipmentMap().get(EquipmentSlot.CHEST));
+        if (grave.getEquipmentMap().containsKey(EquipmentSlot.LEGS))
+            npc.getData().addEquipment(NpcEquipmentSlot.LEGS, grave.getEquipmentMap().get(EquipmentSlot.LEGS));
+        if (grave.getEquipmentMap().containsKey(EquipmentSlot.FEET))
+            npc.getData().addEquipment(NpcEquipmentSlot.FEET, grave.getEquipmentMap().get(EquipmentSlot.FEET));
+    }
+
+    private void equipHands(Npc npc, Grave grave) {
+        if (grave.getEquipmentMap().containsKey(EquipmentSlot.HAND))
+            npc.getData().addEquipment(NpcEquipmentSlot.MAINHAND, grave.getEquipmentMap().get(EquipmentSlot.HAND));
+        if (plugin.getVersionManager().hasSecondHand() && grave.getEquipmentMap().containsKey(EquipmentSlot.OFF_HAND))
+            npc.getData().addEquipment(NpcEquipmentSlot.OFFHAND, grave.getEquipmentMap().get(EquipmentSlot.OFF_HAND));
     }
 
     public void removeCorpse(Grave grave) {
@@ -193,7 +172,30 @@ public final class FancyNPCs extends EntityDataManager {
     public boolean hasCorpse(Grave grave) {
         String id = grave.getUUID().toString().replace("-", "");
         NpcManager npcManager = FancyNpcsPlugin.get().getNpcManager();
-        Npc npc = npcManager.getNpc(id);
-        return npc != null;
+        return npcManager.getNpc(id) != null;
+    }
+
+    /**
+     * Ensures a valid Base64-encoded Minecraft skin texture value for FancyNPCs 2.8.0+.
+     * - If input is already base64 JSON, returns it unchanged.
+     * - If input looks like a URL, wraps it in proper JSON and base64-encodes it.
+     */
+    private static String toValidBase64Texture(String graveTexture) {
+        if (graveTexture == null || graveTexture.isEmpty()) {
+            return "";
+        }
+
+        boolean isAlreadyBase64 = graveTexture.startsWith("eyJ");
+
+        if (isAlreadyBase64) {
+            return graveTexture;
+        }
+
+        String json = String.format(
+                "{\"textures\":{\"SKIN\":{\"url\":\"%s\"}}}",
+                graveTexture
+        );
+        return Base64.getEncoder()
+                .encodeToString(json.getBytes(StandardCharsets.UTF_8));
     }
 }
