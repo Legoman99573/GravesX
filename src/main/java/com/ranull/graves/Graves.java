f79d4401d7b91b7595262e60aef69b918989d05a
Christian W. <7460704+Legoman99573@users.noreply.github.com>
2026-08-31 15:00:31 -0500

    Update Dependencies

--- src/main/java/com/ranull/graves/Graves.java
+++ src/main/java/com/ranull/graves/Graves.java
@@ -402,25 +402,25 @@ public class Graves extends JavaPlugin {
         libraryLoaderUtil.loadLibrary("org{}bstats", "bstats-base", "3.2.1", "org{}bstats", "com{}ranull{}graves{}libraries{}bstats", false);
         libraryLoaderUtil.loadLibrary("org{}bstats", "bstats-bukkit", "3.2.1", "org{}bstats", "com{}ranull{}graves{}libraries{}bstats", false);
 
-        libraryLoaderUtil.loadLibrary("com{}zaxxer", "HikariCP", "7.0.2", "com{}zaxxer{}hikari", "com{}ranull{}graves{}libraries{}hikari", false);
+        libraryLoaderUtil.loadLibrary("com{}zaxxer", "HikariCP", "7.1.0", "com{}zaxxer{}hikari", "com{}ranull{}graves{}libraries{}hikari", false);
         libraryLoaderUtil.loadLibrary("org{}xerial", "sqlite-jdbc", "3.51.2.0", false);
 
         try {
             Class.forName("org.json.JSONObject");
         } catch (ClassNotFoundException e) {
-            libraryLoaderUtil.loadLibrary("org{}json", "json", "20251224");
+            libraryLoaderUtil.loadLibrary("org{}json", "json", "20260814");
         }
 
         try {
             Class.forName("com.google.gson.Gson");
         } catch (ClassNotFoundException e) {
-            libraryLoaderUtil.loadLibrary("com{}google{}code{}gson", "gson", "2.13.2", false);
+            libraryLoaderUtil.loadLibrary("com{}google{}code{}gson", "gson", "2.14.0", false);
         }
 
         try {
             Class.forName("com.google.common.collect.ImmutableList");
         } catch (ClassNotFoundException e) {
-            libraryLoaderUtil.loadLibrary("com{}google{}guava", "guava", "33.5.0-jre", false);
+            libraryLoaderUtil.loadLibrary("com{}google{}guava", "guava", "33.7.1-jre", false);
         }
 
         //libraryLoaderUtil.loadLibrary("com{}github{}oshi", "oshi-core", "6.8.2", false);
@@ -431,23 +431,23 @@ public class Graves extends JavaPlugin {
 
         switch (storageType) {
             case "POSTGRESQL":
-                libraryLoaderUtil.loadLibrary("org{}postgresql", "postgresql", "42.7.11", "org{}postgresql", "com{}ranull{}graves{}libraries{}postgresql", false);
+                libraryLoaderUtil.loadLibrary("org{}postgresql", "postgresql", "42.7.13", "org{}postgresql", "com{}ranull{}graves{}libraries{}postgresql", false);
                 break;
             case "MARIADB":
-                libraryLoaderUtil.loadLibrary("com{}mysql", "mysql-connector-j", "9.6.0", "com{}mysql", "com{}ranull{}graves{}libraries{}mysql", false);
-                libraryLoaderUtil.loadLibrary("org{}mariadb{}jdbc", "mariadb-java-client", "3.5.6", "org{}mariadb", "com{}ranull{}graves{}libraries{}mariadb", false);
+                libraryLoaderUtil.loadLibrary("com{}mysql", "mysql-connector-j", "26.7.0", "com{}mysql", "com{}ranull{}graves{}libraries{}mysql", false);
+                libraryLoaderUtil.loadLibrary("org{}mariadb{}jdbc", "mariadb-java-client", "3.5.10", "org{}mariadb", "com{}ranull{}graves{}libraries{}mariadb", false);
                 break;
             case "MYSQL":
-                libraryLoaderUtil.loadLibrary("com{}mysql", "mysql-connector-j", "9.6.0", "com{}mysql", "com{}ranull{}graves{}libraries{}mysql", false);
+                libraryLoaderUtil.loadLibrary("com{}mysql", "mysql-connector-j", "26.7.0", "com{}mysql", "com{}ranull{}graves{}libraries{}mysql", false);
                 break;
             case "MSSQL":
-                libraryLoaderUtil.loadLibrary("com{}microsoft{}sqlserver", "mssql-jdbc", "13.3.2.jre11-preview", "com{}microsoft", "com{}ranull{}graves{}libraries{}microsoft", false);
+                libraryLoaderUtil.loadLibrary("com{}microsoft{}sqlserver", "mssql-jdbc", "13.5.1.jre11-preview", "com{}microsoft", "com{}ranull{}graves{}libraries{}microsoft", false);
                 break;
         }
         libraryLoaderUtil.loadLibrary("net{}kyori", "adventure-platform-bukkit", "4.4.1", "net{}kyori", "com{}ranull{}graves{}libraries{}kyori", false);
-        libraryLoaderUtil.loadLibrary("net{}kyori", "adventure-api", "4.26.1", "net{}kyori", "com{}ranull{}graves{}libraries{}kyori", false);
-        libraryLoaderUtil.loadLibrary("net{}kyori", "adventure-text-minimessage", "4.26.1", "net{}kyori", "com{}ranull{}graves{}libraries{}kyori", false);
-        libraryLoaderUtil.loadLibrary("net{}kyori", "adventure-text-serializer-gson", "4.26.1", "net{}kyori", "com{}ranull{}graves{}libraries{}kyori", false);
+        libraryLoaderUtil.loadLibrary("net{}kyori", "adventure-api", "5.2.0", "net{}kyori", "com{}ranull{}graves{}libraries{}kyori", false);
+        libraryLoaderUtil.loadLibrary("net{}kyori", "adventure-text-minimessage", "5.2.0", "net{}kyori", "com{}ranull{}graves{}libraries{}kyori", false);
+        libraryLoaderUtil.loadLibrary("net{}kyori", "adventure-text-serializer-gson", "5.2.0", "net{}kyori", "com{}ranull{}graves{}libraries{}kyori", false);
         libraryLoaderUtil.loadLibrary("com{}github{}puregero", "multilib", "1.2.4", "com{}github{}puregero{}multilib", "com{}ranull{}graves{}libraries{}multilib", false, "https://repo.clojars.org/");
         libraryLoaderUtil.loadLibrary("org{}apache{}commons", "commons-text", "1.15.0", "org{}apache{}commons{}text", "com{}ranull{}graves{}libraries{}commonstext", false);
 
